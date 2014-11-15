2c47652c2247625ae610628e88c0eb72a183d3cf
Mitchell Gu <mx.mitchell.gu@gmail.com>
2014-11-15 08:41:46 -0500

    got bentness for all the fingerzgit status!

--- profiles/default.py
+++ profiles/default.py
@@ -9,9 +9,10 @@ def clip(num, a, b):
 
 class DefaultProfile():
 
+  FINGER_IDS = [Finger.TYPE_THUMB, Finger.TYPE_INDEX, Finger.TYPE_MIDDLE, Finger.TYPE_RING, Finger.TYPE_PINKY]
   DISTANCE_GAIN = 1/200.0
   HEIGHT_GAIN = 1/800.0
-  THUMB_GAIN = 10
+  EQ_GAINS = [10, 10, 10, 10, 10]
   source = None
   out = None
 
@@ -19,6 +20,21 @@ class DefaultProfile():
     self.source = source
     self.out = EQ(source, freq=70, q=1, boost=0, mul=0.1)
     self.out.out()
+
+  def getBentness(self, hand0, hand1, fingerID):
+    # Compute angle between thumb and palm normal
+    finger0 = hand0.fingers.finger_type(self.FINGER_IDS[fingerID])[0]
+    finger1 = hand1.fingers.finger_type(self.FINGER_IDS[fingerID])[0]
+    if fingerID == 0:
+      angle0 = hand0.palm_normal.angle_to(finger0.bone(1).direction.cross(hand0.direction))
+      angle1 = hand1.palm_normal.angle_to(finger1.bone(1).direction.cross(hand1.direction))
+    else: 
+      angle0 = hand0.palm_normal.angle_to(finger0.bone(1).direction.cross(hand0.basis.x_basis))
+      angle1 = hand1.palm_normal.angle_to(finger1.bone(1).direction.cross(hand1.basis.x_basis))
+    bentness = 2 - abs(math.pi/2 - angle0) * 2 / math.pi - abs(math.pi/2 - angle1) * 2 / math.pi
+    print "Bentness for FingerID = " + str(fingerID) + ": " + str(bentness)
+    return bentness
+
 
   def step(self, frame):
     if self.out.isPlaying() and len(frame.hands) == 2:
@@ -34,17 +50,11 @@ class DefaultProfile():
       height = hand0.palm_position.y + hand1.palm_position.y
       print "Height: " + str(height)
 
-      # Compute angle between thumb and palm normal
-      thumb0 = hand0.fingers.finger_type(Finger.TYPE_THUMB)[0]
-      thumb1 = hand1.fingers.finger_type(Finger.TYPE_THUMB)[0]
-      angle0 = hand0.palm_normal.angle_to(thumb0.bone(1).direction.cross(hand0.direction))
-      angle1 = hand1.palm_normal.angle_to(thumb1.bone(1).direction.cross(hand1.direction))
-      bentness0 = 1-abs(math.pi/2 - angle0) * 2 / math.pi
-      bentness1 = 1-abs(math.pi/2 - angle1) * 2 / math.pi
-      #print "Bentness 0: " + str(bentness0)
-      #print "Bentness 1: " + str(bentness1)
-      self.out.setBoost(self.THUMB_GAIN * (bentness0 + bentness1 - 1))
-      print "Boost: " + str(self.THUMB_GAIN * (bentness0 + bentness1 - 1))
+      for fingerID in range(len(self.FINGER_IDS)):
+        bentness = self.getBentness(hand0, hand1, fingerID)
+        if fingerID == 0:
+          self.out.setBoost(self.EQ_GAINS[fingerID] * (bentness - 1))
+          print "Bass Boost: " + str(self.EQ_GAINS[fingerID] * (bentness - 1))
 
       # Set playback speed proportional to distance
       self.source.setSpeed(distance * self.DISTANCE_GAIN)
