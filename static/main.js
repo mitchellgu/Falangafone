22f2a5f42f5523d851b1091f6192621d75ed0867
Mitchell Gu <mx.mitchell.gu@gmail.com>
2014-11-15 23:31:02 -0500

    sliders work!

--- static/main.js
+++ static/main.js
@@ -5,9 +5,11 @@ $(function() {
    setTimeout(function() {
        $.ajax({ url: "http://131a46b7.ngrok.com/params", success: function(data) {
             $("#volume").text(data.volume);
+            $("#volume-bar").css("width", data.volume + "%");
             $("#speed").text(data.speed)
+            $("#speed-bar").css("width", String(parseInt(data.speed)/2.0) + "%");
        }, dataType: "json", complete: poll });
-    }, 250);
+    }, 150);
 })();
 
 });
